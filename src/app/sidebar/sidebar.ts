75483fa590219091269476b2fbf2c0c44a81ad55
ousmane sow <thifuge@gmail.com>
2026-08-10 14:09:02 +0200

    feat(sidebar): implement sidebar component

--- src/app/sidebar/sidebar.ts
+++ src/app/sidebar/sidebar.ts
@@ -1,9 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+
+import { ButtonModule } from 'primeng/button';
+
+import { PIcon } from '@primeicons/angular/p-icon';
 
 @Component({
   selector: 'app-sidebar',
-  imports: [],
+  imports: [ ButtonModule, PIcon ],
   templateUrl: './sidebar.html',
   styleUrl: './sidebar.scss',
 })
-export class Sidebar {}
+export class Sidebar{
+
+
+ NAV_LINKS = [
+  {
+    id: 0,
+    label: 'Dashboard', 
+    icon: 'home'
+  },
+  {
+    id: 1,
+    label: 'Commandes', 
+    icon: 'shopping-cart'
+  },
+  {
+    id: 2,
+    label: 'Clients', 
+    icon: 'users'
+  },
+  {
+    id: 3,
+    label: 'Catalogue', 
+    icon: 'shop'
+  },
+  {
+    id: 4,
+    label: 'Historique', 
+    icon: 'history'
+  }
+  
+ ]
+  
+ LINS = signal(this.NAV_LINKS);
+
+}
