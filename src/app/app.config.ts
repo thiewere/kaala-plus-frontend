42503dbd3296a622437629e724e061f4e3859b6c
ousmane sow <thifuge@gmail.com>
2026-08-10 14:05:22 +0200

    config: add community licence for primeui

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -15,8 +15,10 @@ export const appConfig: ApplicationConfig = {
                 preset: Aura,
                 options: {
                   darkModeSelector: '.app-dark'
-                }
-            }
+                },
+            },
+      license: "eyJpZCI6IjY0NWRlNzUwLTk5YTAtNGZhNi05NDAzLTVlNzVkNWQ5MzY5OCIsInByb2R1Y3QiOiJwcmltZXVpIiwidGllciI6ImNvbW11bml0eSIsInR5cGUiOiJkZXYiLCJpYXQiOjE3ODYwMDMzNjEsImV4cCI6MTgxNzUzOTM2MX0.3m56w7ht45cGkkrDa2cw3ao0-GNYR9JUBCdNP9-MzG8tj-ZwuFE7czsHSx_TN-BJW6kMKF6yScHFme3v_eaKAA"
+      
     })
   ]
 };
