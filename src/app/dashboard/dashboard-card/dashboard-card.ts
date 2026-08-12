0bef5b1e09cd4b5c60644168837656f32a3a61f0
ousmane sow <thifuge@gmail.com>
2026-08-12 18:41:39 +0200

    feat(dashboard/card): implement card design

--- src/app/dashboard/dashboard-card/dashboard-card.ts
+++ src/app/dashboard/dashboard-card/dashboard-card.ts
@@ -1,9 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, computed, input } from '@angular/core';
+import { PIcon } from '@primeicons/angular/p-icon';
+import { DashboardCardData } from './dashboard-card-data';
 
 @Component({
   selector: 'app-dashboard-card',
-  imports: [],
+  imports: [PIcon],
   templateUrl: './dashboard-card.html',
   styleUrl: './dashboard-card.scss',
 })
-export class DashboardCard {}
+export class DashboardCard {
+  card = input.required<DashboardCardData>();
+
+}
