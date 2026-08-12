7a008b6376ff95afb63958c255f1a5bb992f960a
ousmane sow <thifuge@gmail.com>
2026-08-12 17:00:58 +0200

    feat(dashboard/card): generate dashboard-card component

---
+++ src/app/dashboard/dashboard-card/dashboard-card.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard-card',
+  imports: [],
+  templateUrl: './dashboard-card.html',
+  styleUrl: './dashboard-card.scss',
+})
+export class DashboardCard {}
