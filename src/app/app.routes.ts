ac897728139415fb4374f839f67ea8fe104984e9
ousmane sow <thifuge@gmail.com>
2026-08-12 20:28:35 +0200

    feat(order-list): add orders path to routes array

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { Dashboard } from './dashboard/dashboard';
+import { OrderList } from './orders/order-list/order-list';
 
 export const routes: Routes = [
     {path: '', component: Dashboard},
+    {path: 'orders', component: OrderList},
     
 ];
