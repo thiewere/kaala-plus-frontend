7627cc51e08e7707ad2d930f6fb009b69deaa0b6
ousmane sow <thifuge@gmail.com>
2026-08-12 20:23:38 +0200

    feat(order-list): generate order-list component

---
+++ src/app/orders/order-list/order-list.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-order-list',
+  imports: [],
+  templateUrl: './order-list.html',
+  styleUrl: './order-list.scss',
+})
+export class OrderList {}
