29f0ae2f58d82094ddecaa999e25f85426439ca5
ousmane sow <thifuge@gmail.com>
2026-08-13 11:09:17 +0200

    feat(order-list): implement header component into order list

--- src/app/orders/order-list/order-list.ts
+++ src/app/orders/order-list/order-list.ts
@@ -1,9 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+
+import { Header } from "../../header/header";
+import { HeaderData } from '../../header/header-data.model';
 
 @Component({
   selector: 'app-order-list',
-  imports: [],
+  imports: [Header],
   templateUrl: './order-list.html',
   styleUrl: './order-list.scss',
 })
-export class OrderList {}
+export class OrderList {
+
+  protected readonly headerData = signal<HeaderData>({title: "Liste des commandes", icon: "shopping-cart"})
+
+}
