46060b947eecc3b3007c531ff72d6866dcc3fc88
ousmane sow <thifuge@gmail.com>
2026-08-13 18:45:07 +0200

    feat(client-service): implement ClientService to fetch data from backend

--- src/app/clients/client-service.ts
+++ src/app/clients/client-service.ts
@@ -1,6 +1,17 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Client } from './client-list/client.model';
 
 @Injectable({
   providedIn: 'root',
 })
-export class ClientService {}
+export class ClientService {
+
+  httpClient = inject(HttpClient);
+
+  baseUrl = "http://localhost:8080/api";
+
+  getClients() {
+    return this.httpClient.get<Client[]>(`${"http://localhost:8080/api"}/clients`);
+  }
+}
