e624eea979d5f107096608b41eea50700306ee6e
ousmane sow <thifuge@gmail.com>
2026-08-13 13:06:03 +0200

    feat(recherche-add): add initial component with template and styles

---
+++ src/app/recherche-add/recherche-add.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-recherche-add',
+  imports: [],
+  templateUrl: './recherche-add.html',
+  styleUrl: './recherche-add.scss',
+})
+export class RechercheAdd {}
