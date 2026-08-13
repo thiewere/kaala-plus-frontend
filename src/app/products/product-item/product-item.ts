fd5577778f13210edd7c6eaf0201bd43528593d7
ousmane sow <thifuge@gmail.com>
2026-08-13 23:47:39 +0200

    feat(product-item): add ProductItem component with template, styles, and tests

---
+++ src/app/products/product-item/product-item.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-item',
+  imports: [],
+  templateUrl: './product-item.html',
+  styleUrl: './product-item.scss',
+})
+export class ProductItem {}
