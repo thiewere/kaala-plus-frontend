0fb540135b6657200943ca39554441afb9cf2bfd
ousmane sow <thifuge@gmail.com>
2026-08-13 23:36:01 +0200

    feat(product-service): implement ProductService to fetch data from backend

--- src/app/products/product-service.ts
+++ src/app/products/product-service.ts
@@ -1,6 +1,17 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { environment } from '../../environments/environment.development';
+import { Product } from './product.model';
 
 @Injectable({
   providedIn: 'root',
 })
-export class ProductService {}
+export class ProductService {
+  httpClien = inject(HttpClient);
+
+  baseUrl = environment.apiUrl + 'products'
+
+  getProducts(){
+    return this.httpClien.get<Product[]>(this.baseUrl);
+  }
+}
