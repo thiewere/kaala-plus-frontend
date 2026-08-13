1ad4bdb635daaa33bb450907a5013a02f99152da
ousmane sow <thifuge@gmail.com>
2026-08-13 11:22:38 +0200

    feat(order-list): inject OrderService and fetch order products in constructor

--- src/app/orders/order-list/order-list.ts
+++ src/app/orders/order-list/order-list.ts
@@ -1,4 +1,6 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { OrderService } from '../order-service';
+import { OrderProduct } from '../order.model';
 
 import { ButtonModule } from 'primeng/button';
 import { Header } from "../../header/header";
@@ -11,7 +13,16 @@ import { HeaderData } from '../../header/header-data.model';
   styleUrl: './order-list.scss',
 })
 export class OrderList {
+  orderService = inject(OrderService);
+
+  orders = signal<OrderProduct[]>([]);
 
   protected readonly headerData = signal<HeaderData>({title: "Liste des commandes", icon: "shopping-cart"})
 
+  constructor() {
+    this.orderService.getOrderProducts().subscribe((data) => {
+      this.orders.set(data)
+    })
+  }
+  
 }
