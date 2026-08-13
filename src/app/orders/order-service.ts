f3304b4c39a842b93db5cdb095e1110c6233b507
ousmane sow <thifuge@gmail.com>
2026-08-13 11:20:29 +0200

    feat(order-service): http request to get all orderProducts from the backend

--- src/app/orders/order-service.ts
+++ src/app/orders/order-service.ts
@@ -1,6 +1,16 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { OrderProduct } from './order.model';
 
 @Injectable({
   providedIn: 'root',
 })
-export class OrderService {}
+export class OrderService {
+  httpClient = inject(HttpClient);
+
+  baseUrl = "http://localhost:8080/api"
+
+  getOrderProducts() {
+    return this.httpClient.get<OrderProduct[]>(`${this.baseUrl}/order_items`)
+  }
+}
