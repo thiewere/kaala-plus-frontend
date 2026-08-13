cad793199e8e30c0c0682ffb7bf7bc77e72583d4
ousmane sow <thifuge@gmail.com>
2026-08-13 12:47:42 +0200

    fix(sidebar): update sidebar links to use routerLink for navigation

--- src/app/sidebar/sidebar.ts
+++ src/app/sidebar/sidebar.ts
@@ -5,10 +5,11 @@ import { ButtonModule } from 'primeng/button';
 import { PIcon } from '@primeicons/angular/p-icon';
 import { SidebarItemData } from './sidebar-item-data';
 import { SIDEBAR_ITEMS } from './data/sidebar-items.data';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-sidebar',
-  imports: [ ButtonModule, PIcon ],
+  imports: [ButtonModule, PIcon, RouterLink],
   templateUrl: './sidebar.html',
   styleUrl: './sidebar.scss',
 })
