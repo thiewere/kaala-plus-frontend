7088a6685f050fd82ed68874c20c1784dbe7e1cc
ousmane sow <thifuge@gmail.com>
2026-08-13 12:51:58 +0200

    config(routes): add route for ClientList component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { Dashboard } from './dashboard/dashboard';
 import { OrderList } from './orders/order-list/order-list';
+import { ClientList } from './clients/client-list/client-list';
 
 export const routes: Routes = [
     {path: '', component: Dashboard},
     {path: 'orders', component: OrderList},
+    {path: 'clients', component: ClientList},
     
 ];
