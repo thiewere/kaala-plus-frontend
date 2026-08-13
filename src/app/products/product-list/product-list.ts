8c3c704eda61f97a7a02656abe94ecfc2a3383fc
ousmane sow <thifuge@gmail.com>
2026-08-13 19:15:58 +0200

    feat(product-list): add RechercheAdd component and update template with search functionality

--- src/app/products/product-list/product-list.ts
+++ src/app/products/product-list/product-list.ts
@@ -1,14 +1,17 @@
 import { Component, signal } from '@angular/core';
 import { Header } from "../../header/header";
 import { HeaderData } from '../../header/header-data.model';
+import { RechercheAddData } from '../../recherche-add/recherche-add-data.model';
+import { RechercheAdd } from "../../recherche-add/recherche-add";
 
 @Component({
   selector: 'app-product-list',
-  imports: [Header],
+  imports: [Header, RechercheAdd],
   templateUrl: './product-list.html',
   styleUrl: './product-list.scss',
 })
 export class ProductList {
   protected readonly headerData = signal<HeaderData>({title: 'Liste des Produits', icon: 'box'});
 
+ protected readonly searchAddFieldData = signal<RechercheAddData>({placeholder: "Rechercher par le nom", label: "Nouveau Produit"})
 }
