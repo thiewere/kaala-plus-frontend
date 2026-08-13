1bb31f113a5b95edd967e8be9e0896f6844e017a
ousmane sow <thifuge@gmail.com>
2026-08-13 13:25:49 +0200

    feat(client-list): add RechercheAdd component and update client-list template

--- src/app/clients/client-list/client-list.ts
+++ src/app/clients/client-list/client-list.ts
@@ -1,14 +1,18 @@
 import { Component, signal } from '@angular/core';
 import { Header } from "../../header/header";
 import { HeaderData } from '../../header/header-data.model';
+import { RechercheAdd } from '../../recherche-add/recherche-add';
+import { RechercheAddData } from '../../recherche-add/recherche-add-data.model';
 
 @Component({
   selector: 'app-client-list',
-  imports: [Header],
+  imports: [Header, RechercheAdd],
   templateUrl: './client-list.html',
   styleUrl: './client-list.scss',
 })
 export class ClientList {
 
   protected readonly headerData = signal<HeaderData>({title: 'Liste des Clients', icon: 'users'});
+
+  protected readonly searchAddFieldData = signal<RechercheAddData>({placeholder: "Rechercher par le nom", label: "Nouveau Client"})
 }
