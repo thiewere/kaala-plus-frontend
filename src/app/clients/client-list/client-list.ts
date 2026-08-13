b15d0ab880f7f2117cb884fcce72cb1388af2adb
ousmane sow <thifuge@gmail.com>
2026-08-13 18:50:49 +0200

    feat(client-list): enhance client list with dynamic data and table structure

--- src/app/clients/client-list/client-list.ts
+++ src/app/clients/client-list/client-list.ts
@@ -1,12 +1,16 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { Header } from "../../header/header";
 import { HeaderData } from '../../header/header-data.model';
 import { RechercheAdd } from '../../recherche-add/recherche-add';
 import { RechercheAddData } from '../../recherche-add/recherche-add-data.model';
+import { ClientService } from '../client-service';
+import { Client } from './client.model';
+import { TableModule } from 'primeng/table';
+import { ClientItem } from "../client-item/client-item";
 
 @Component({
   selector: 'app-client-list',
-  imports: [Header, RechercheAdd],
+  imports: [Header, RechercheAdd, TableModule, ClientItem],
   templateUrl: './client-list.html',
   styleUrl: './client-list.scss',
 })
@@ -15,4 +19,13 @@ export class ClientList {
   protected readonly headerData = signal<HeaderData>({title: 'Liste des Clients', icon: 'users'});
 
   protected readonly searchAddFieldData = signal<RechercheAddData>({placeholder: "Rechercher par le nom", label: "Nouveau Client"})
+
+  clientService = inject(ClientService);
+
+  clients = signal<Client[]>([]);
+
+  constructor() {
+    this.clientService.getClients().subscribe((client) => this.clients.set(client));
+    console.log(this.clients())
+  }
 }
