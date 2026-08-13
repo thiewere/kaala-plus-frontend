2aa0fdbac02ee7bc350294dfc9b134361b903eb1
ousmane sow <thifuge@gmail.com>
2026-08-13 19:14:00 +0200

    feat(product-list): update template and add header component with dynamic data

--- src/app/products/product-list/product-list.ts
+++ src/app/products/product-list/product-list.ts
@@ -1,9 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { Header } from "../../header/header";
+import { HeaderData } from '../../header/header-data.model';
 
 @Component({
   selector: 'app-product-list',
-  imports: [],
+  imports: [Header],
   templateUrl: './product-list.html',
   styleUrl: './product-list.scss',
 })
-export class ProductList {}
+export class ProductList {
+  protected readonly headerData = signal<HeaderData>({title: 'Liste des Produits', icon: 'box'});
+
+}
