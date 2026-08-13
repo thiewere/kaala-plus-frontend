e3e31c3b12316ecd148914f5a6eca1e9d89e64d4
ousmane sow <thifuge@gmail.com>
2026-08-13 11:43:13 +0200

    feat(order-item): generate order item component

---
+++ src/app/orders/order-item/order-item.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-order-item',
+  imports: [],
+  templateUrl: './order-item.html',
+  styleUrl: './order-item.scss',
+})
+export class OrderItem {}
