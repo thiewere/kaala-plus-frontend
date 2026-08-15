a35472e72ece1cff2301bdf2c2bc8c00bc2ee44a
ousmane sow <thifuge@gmail.com>
2026-08-15 12:08:34 +0200

    feat(routes): add route for SupplierList component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { Dashboard } from './dashboard/dashboard';
 import { OrderList } from './orders/order-list/order-list';
 import { ClientList } from './clients/client-list/client-list';
 import { ProductList } from './products/product-list/product-list';
+import { SupplierList } from './suppliers/supplier-list/supplier-list';
 
 export const routes: Routes = [
     {path: '', component: Dashboard},
     {path: 'orders', component: OrderList},
     {path: 'clients', component: ClientList},
     {path: 'products', component: ProductList},
+    {path: 'suppliers', component: SupplierList},
     
 ];
