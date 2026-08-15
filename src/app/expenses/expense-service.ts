9cf66825de277d4a8bd40c535aac9ae957419143
ousmane sow <thifuge@gmail.com>
2026-08-15 16:27:24 +0200

    feat(expense-service): implement ExpenseService with getExpenses method

--- src/app/expenses/expense-service.ts
+++ src/app/expenses/expense-service.ts
@@ -1,6 +1,17 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { environment } from '../../environments/environment.development';
+import { Payment } from '../payments/payment.model';
 
 @Injectable({
   providedIn: 'root',
 })
-export class ExpenseService {}
+export class ExpenseService {
+  httpCient = inject(HttpClient);
+  
+  baseUrl = environment.apiUrl + 'expenses';
+
+  getExpenses() {
+    return this.httpCient.get<Payment[]>(this.baseUrl);
+  }
+}
