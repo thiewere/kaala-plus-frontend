765509db7d00759b5b86010e74eb32db1cb1cdfb
ousmane sow <thifuge@gmail.com>
2026-08-15 16:23:14 +0200

    feat(expense-list): integrate Header component and set up header data

--- src/app/expenses/expense-list/expense-list.ts
+++ src/app/expenses/expense-list/expense-list.ts
@@ -1,9 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { HeaderData } from '../../header/header-data.model';
+import { Header } from "../../header/header";
 
 @Component({
   selector: 'app-expense-list',
-  imports: [],
+  imports: [Header],
   templateUrl: './expense-list.html',
   styleUrl: './expense-list.scss',
 })
-export class ExpenseList {}
+export class ExpenseList {
+  protected readonly headerData = signal<HeaderData>({title: "Liste des Dépenses", icon: "wallet"})
+ 
+}
