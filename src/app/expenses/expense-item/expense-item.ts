4480a8ffef777408bded17d9ea26bafb09167c86
ousmane sow <thifuge@gmail.com>
2026-08-15 16:36:27 +0200

    feat(expense-item): add ExpenseItem component with template, styles, and tests

---
+++ src/app/expenses/expense-item/expense-item.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-expense-item',
+  imports: [],
+  templateUrl: './expense-item.html',
+  styleUrl: './expense-item.scss',
+})
+export class ExpenseItem {}
