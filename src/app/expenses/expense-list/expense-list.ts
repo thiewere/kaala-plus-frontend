de1fdd461f72cd79f770be68a1b4bd9335ad286b
ousmane sow <thifuge@gmail.com>
2026-08-15 16:11:54 +0200

    feat(expense-list): add initial ExpenseList component with template, styles, and tests

---
+++ src/app/expenses/expense-list/expense-list.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-expense-list',
+  imports: [],
+  templateUrl: './expense-list.html',
+  styleUrl: './expense-list.scss',
+})
+export class ExpenseList {}
