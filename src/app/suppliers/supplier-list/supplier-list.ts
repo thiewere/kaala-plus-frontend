4541fc5321a9afe7ea7901148fef580d802c8493
ousmane sow <thifuge@gmail.com>
2026-08-15 12:22:29 +0200

    feat(supplier-list): integrate p-table for supplier listing

--- src/app/suppliers/supplier-list/supplier-list.ts
+++ src/app/suppliers/supplier-list/supplier-list.ts
@@ -3,10 +3,11 @@ import { HeaderData } from '../../header/header-data.model';
 import { Header } from "../../header/header";
 import { RechercheAddData } from '../../recherche-add/recherche-add-data.model';
 import { RechercheAdd } from "../../recherche-add/recherche-add";
+import { TableModule } from 'primeng/table';
 
 @Component({
   selector: 'app-supplier-list',
-  imports: [Header, RechercheAdd],
+  imports: [Header, RechercheAdd, TableModule],
   templateUrl: './supplier-list.html',
   styleUrl: './supplier-list.scss',
 })
