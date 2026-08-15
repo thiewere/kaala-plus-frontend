5784d2e7ad13247ed6b32f62f1743a66babb8490
ousmane sow <thifuge@gmail.com>
2026-08-15 12:29:20 +0200

    feat(supplier-service): implement SupplierService with getSuppliers method

--- src/app/suppliers/supplier-service.ts
+++ src/app/suppliers/supplier-service.ts
@@ -1,6 +1,17 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { environment } from '../../environments/environment.development';
+import { SupplierProduct } from './supplier.model';
 
 @Injectable({
   providedIn: 'root',
 })
-export class SupplierService {}
+export class SupplierService {
+  httpClient = inject(HttpClient);
+
+  baseUrl = environment.apiUrl + 'supplier_products';
+
+  getSuppliers() {
+    return this.httpClient.get<SupplierProduct[]>(this.baseUrl);
+  }
+}
