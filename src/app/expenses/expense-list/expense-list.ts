6547e4290712c5a3d7fc8dd5821cf099e0413e85
ousmane sow <thifuge@gmail.com>
2026-08-15 16:28:51 +0200

    feat(expense-list): inject ExpenseService and convert expenses to signal

--- src/app/expenses/expense-list/expense-list.ts
+++ src/app/expenses/expense-list/expense-list.ts
@@ -1,8 +1,10 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { HeaderData } from '../../header/header-data.model';
 import { Header } from "../../header/header";
 import { RechercheAddData } from '../../recherche-add/recherche-add-data.model';
 import { RechercheAdd } from "../../recherche-add/recherche-add";
+import { ExpenseService } from '../expense-service';
+import { toSignal } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-expense-list',
@@ -11,7 +13,10 @@ import { RechercheAdd } from "../../recherche-add/recherche-add";
   styleUrl: './expense-list.scss',
 })
 export class ExpenseList {
-  protected readonly headerData = signal<HeaderData>({title: "Liste des Dépenses", icon: "wallet"})
-  protected readonly searchAddFieldData = signal<RechercheAddData>({placeholder: "Rechercher par type", label: "Nouvelle Dépense"})
+  protected readonly headerData = signal<HeaderData>({title: "Liste des Dépenses", icon: "wallet"});
+  protected readonly searchAddFieldData = signal<RechercheAddData>({placeholder: "Rechercher par type", label: "Nouvelle Dépense"});
+
+  expenseService = inject(ExpenseService);
+  expenses = toSignal(this.expenseService.getExpenses());
  
 }
