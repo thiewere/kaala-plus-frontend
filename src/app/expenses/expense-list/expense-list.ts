55ee417bd12cdad4942655a601e1e358379eb90e
ousmane sow <thifuge@gmail.com>
2026-08-15 16:24:54 +0200

    feat(expense-list): add RechercheAdd component

--- src/app/expenses/expense-list/expense-list.ts
+++ src/app/expenses/expense-list/expense-list.ts
@@ -1,14 +1,17 @@
 import { Component, signal } from '@angular/core';
 import { HeaderData } from '../../header/header-data.model';
 import { Header } from "../../header/header";
+import { RechercheAddData } from '../../recherche-add/recherche-add-data.model';
+import { RechercheAdd } from "../../recherche-add/recherche-add";
 
 @Component({
   selector: 'app-expense-list',
-  imports: [Header],
+  imports: [Header, RechercheAdd],
   templateUrl: './expense-list.html',
   styleUrl: './expense-list.scss',
 })
 export class ExpenseList {
   protected readonly headerData = signal<HeaderData>({title: "Liste des Dépenses", icon: "wallet"})
+  protected readonly searchAddFieldData = signal<RechercheAddData>({placeholder: "Rechercher par type", label: "Nouvelle Dépense"})
  
 }
