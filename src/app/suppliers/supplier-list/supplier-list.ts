226cc30c8c7867c7f4cae6cf76adac030116a63d
ousmane sow <thifuge@gmail.com>
2026-08-15 12:22:14 +0200

    feat(supplier-list): add RechercheAdd component

--- src/app/suppliers/supplier-list/supplier-list.ts
+++ src/app/suppliers/supplier-list/supplier-list.ts
@@ -1,16 +1,20 @@
 import { Component, signal } from '@angular/core';
 import { HeaderData } from '../../header/header-data.model';
 import { Header } from "../../header/header";
+import { RechercheAddData } from '../../recherche-add/recherche-add-data.model';
+import { RechercheAdd } from "../../recherche-add/recherche-add";
 
 @Component({
   selector: 'app-supplier-list',
-  imports: [Header],
+  imports: [Header, RechercheAdd],
   templateUrl: './supplier-list.html',
   styleUrl: './supplier-list.scss',
 })
 export class SupplierList {
 
-  protected readonly headerData = signal<HeaderData>({title: "Liste des Fournisseurs", icon: "building"})
+  protected readonly headerData = signal<HeaderData>({title: "Liste des Fournisseurs", icon: "building"});
+
+  protected readonly searchAddFieldData = signal<RechercheAddData>({placeholder: "Rechercher par le nom", label: "Nouveau Fournisseur"});
  
 
 }
